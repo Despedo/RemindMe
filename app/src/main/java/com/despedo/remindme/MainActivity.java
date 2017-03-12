334a6a4fc22c3f16d80614cdbf6696929f0ada9a
Despedo <despedo@gmail.com>
2017-03-12 19:00:41 +0200

    Created MainActivity and layout

---
+++ app/src/main/java/com/despedo/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.despedo.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Despedo on 12.03.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
